ebbb3e188573f096e83063717c50b6a2f6c9b18d
Shruti Singh <76986171+Shruti6399@users.noreply.github.com>
2023-01-22 16:49:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// alert("Welcome to Dice Challenge")
+var player1 = prompt("Enter player 1 name");
+var player2 = prompt("Enter player 2 name")
+document.querySelector(".p1").innerHTML = player1;
+document.querySelector(".p2").innerHTML = player2;
+ function random(){
+     var n = Math.random();
+     n = Math.floor(n * 6)+1;
+     return n;
+ }
+ random();
+
+var imagesArray = ["images/dice1.png" ,"images/dice2.png" ,"images/dice3.png",
+                    "images/dice4.png","images/dice5.png" ,"images/dice6.png" ];
+
+function displayimage(){
+    var a = random();
+    document.canvas.src = imagesArray[a-1];
+    var b = random();
+    document.canvas2.src = imagesArray[b-1]
+    // alert(n);
+    if(a>b){
+        document.querySelector("h1").innerHTML = "👑" + player1 + " Wins";
+    }
+    else if(b>a){
+        document.querySelector("h1").innerHTML = player2 + " Wins 👑";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "It's a Draw!!!";
+    }
+}
+
